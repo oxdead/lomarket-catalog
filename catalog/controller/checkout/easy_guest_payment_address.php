6503b1b9000fc52b1e04e94fa0fb66cc4293e93a
oxdead <som0@meta.ua>
2021-02-26 12:53:16 +0200

    checkout save

--- catalog/controller/checkout/easy_guest_payment_address.php
+++ catalog/controller/checkout/easy_guest_payment_address.php
@@ -268,10 +268,11 @@ class ControllerCheckoutEasyGuestPaymentAddress extends Controller {
 				}
 			}
 
-			unset($this->session->data['shipping_method']);
-			unset($this->session->data['shipping_methods']);
-			unset($this->session->data['payment_method']);
-			unset($this->session->data['payment_methods']);
+			
+			//unset($this->session->data['shipping_method']);
+			//unset($this->session->data['shipping_methods']);
+			//unset($this->session->data['payment_method']);
+			//unset($this->session->data['payment_methods']);
 		}
 
 
--- catalog/controller/checkout/easy_guest_shipping_method.php
+++ catalog/controller/checkout/easy_guest_shipping_method.php
@@ -96,62 +96,66 @@ class ControllerCheckoutEasyGuestShippingMethod extends Controller {
 		$this->response->setOutput($this->load->view('checkout/easy_guest_shipping_method', $data));
 	}
 
-	// public function save() {
-	// 	$this->load->language('checkout/checkout');
+	public function save() {
+		$this->load->language('checkout/checkout');
 
-	// 	$json = array();
+		$json = array();
 
-	// 	// Validate if shipping is required. If not the customer should not have reached this page.
-	// 	if (!$this->cart->hasShipping()) {
-	// 		$json['redirect'] = $this->url->link('checkout/checkout', '', true);
-	// 	}
+		// Validate if shipping is required. If not the customer should not have reached this page.
+		if (!$this->cart->hasShipping()) {
+			$json['redirect'] = $this->url->link('checkout/checkout', '', true);
+		}
 
-	// 	// Validate if shipping address has been set.
-	// 	if (!isset($this->session->data['shipping_address'])) {
-	// 		$json['redirect'] = $this->url->link('checkout/checkout', '', true);
-	// 	}
+		// Validate if shipping address has been set.
+		if (!isset($this->session->data['shipping_address'])) {
+			$json['redirect'] = $this->url->link('checkout/checkout', '', true);
+		}
 
-	// 	// Validate cart has products and has stock.
-	// 	if ((!$this->cart->hasProducts() && empty($this->session->data['vouchers'])) || (!$this->cart->hasStock() && !$this->config->get('config_stock_checkout'))) {
-	// 		$json['redirect'] = $this->url->link('checkout/cart');
-	// 	}
+		// Validate cart has products and has stock.
+		if ((!$this->cart->hasProducts() && empty($this->session->data['vouchers'])) || (!$this->cart->hasStock() && !$this->config->get('config_stock_checkout'))) {
+			$json['redirect'] = $this->url->link('checkout/cart');
+		}
 
-	// 	// Validate minimum quantity requirements.
-	// 	$products = $this->cart->getProducts();
+		// Validate minimum quantity requirements.
+		$products = $this->cart->getProducts();
 
-	// 	foreach ($products as $product) {
-	// 		$product_total = 0;
+		foreach ($products as $product) {
+			$product_total = 0;
 
-	// 		foreach ($products as $product_2) {
-	// 			if ($product_2['product_id'] == $product['product_id']) {
-	// 				$product_total += $product_2['quantity'];
-	// 			}
-	// 		}
+			foreach ($products as $product_2) {
+				if ($product_2['product_id'] == $product['product_id']) {
+					$product_total += $product_2['quantity'];
+				}
+			}
 
-	// 		if ($product['minimum'] > $product_total) {
-	// 			$json['redirect'] = $this->url->link('checkout/cart');
+			if ($product['minimum'] > $product_total) {
+				$json['redirect'] = $this->url->link('checkout/cart');
 
-	// 			break;
-	// 		}
-	// 	}
+				break;
+			}
+		}
 
-	// 	if (!isset($this->request->post['shipping_method'])) {
-	// 		$json['error']['warning'] = $this->language->get('error_shipping');
-	// 	} else {
-	// 		$shipping = explode('.', $this->request->post['shipping_method']);
+		
+		if (!isset($this->request->post['shipping_method'])) {
+			$json['error']['warning'] = $this->language->get('error_shipping');
+		} else {
+			$shipping = explode('.', $this->request->post['shipping_method']);
+			
 
-	// 		if (!isset($shipping[0]) || !isset($shipping[1]) || !isset($this->session->data['shipping_methods'][$shipping[0]]['quote'][$shipping[1]])) {
-	// 			$json['error']['warning'] = $this->language->get('error_shipping');
-	// 		}
-	// 	}
+			if (!isset($shipping[0]) || !isset($shipping[1]) || !isset($this->session->data['shipping_methods'][$shipping[0]]['quote'][$shipping[1]])) {
+				$json['error']['warning'] = $this->language->get('error_shipping');
+			}
+		}
 
-	// 	if (!$json) {
-	// 		$this->session->data['shipping_method'] = $this->session->data['shipping_methods'][$shipping[0]]['quote'][$shipping[1]];
+		
 
-	// 		$this->session->data['comment'] = strip_tags($this->request->post['comment']);
-	// 	}
+		if (!$json) {
+			$this->session->data['shipping_method'] = $this->session->data['shipping_methods'][$shipping[0]]['quote'][$shipping[1]];
 
-	// 	$this->response->addHeader('Content-Type: application/json');
-	// 	$this->response->setOutput(json_encode($json));
-	// }
+			$this->session->data['comment'] = strip_tags($this->request->post['comment']);
+		}
+
+		$this->response->addHeader('Content-Type: application/json');
+		$this->response->setOutput(json_encode($json));
+	}
 }
